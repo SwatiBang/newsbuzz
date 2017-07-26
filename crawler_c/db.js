326f30528b550e57ac7f67ecc57d0410f9d41782
SwatiBang <swati.3rdstar@gmail.com>
2017-07-25 17:19:42 -0700

    Initial integrated newsbuzz capstone.

---
+++ crawler_c/db.js
@@ -0,0 +1,10 @@
+// db.js
+module.exports = function() {
+  return {
+    politics0: require('./crawler/politics0.json'),
+    politics1: require('./crawler/politics1.json'),
+	politics2: require('./crawler/politics2.json'),
+    politics3: require('./crawler/politics3.json'),
+	politics4: require('./crawler/politics4.json'),
+  }
+}
